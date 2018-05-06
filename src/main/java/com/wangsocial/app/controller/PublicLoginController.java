f8b93afecaf8b6e0cb5e841de0360fa68316ca63
253847574@qq.com <wang7895123640>
2018-05-06 16:59:03 +0800

    登陆

---
+++ src/main/java/com/wangsocial/app/controller/PublicLoginController.java
@@ -0,0 +1,23 @@
+package com.wangsocial.app.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class PublicLoginController {
+
+	@RequestMapping(value = "/login")
+	public String Print( String username,String password,String verify) {
+		Map<String, Object> map = new HashMap<>();
+		if (username.equals("123")&&password.equals("123")) {
+			return "index1";
+		}
+		map.put("success", "1");
+		return "Public/login";
+	}
+}
